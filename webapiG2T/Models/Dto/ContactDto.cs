30278a046e73c4299d23e24895305227252a4e92
Assane DOUMBOUYA <assane.doumbouya@atos.net>
2024-07-15 16:30:52 +0000

    ajout des dto

---
+++ webapiG2T/Models/Dto/ContactDto.cs
@@ -0,0 +1,15 @@
+﻿namespace webapiG2T.Models.Dto
+{
+    public class ContactDto
+    {
+
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Telephone { get; set; }
+        public string Adresse { get; set; }
+        public string StatutContact { get; set; }
+        public int CompteId { get; set; }
+
+    }
+}
---
+++ webapiG2T/Models/Dto/IncidentDto.cs
@@ -0,0 +1,15 @@
+﻿namespace webapiG2T.Models.Dto
+{
+    public class IncidentDto
+    {
+        public int Id { get; set; }
+        public string Canal { get; set; }
+        public string Motif { get; set; }
+        public string SousMotif { get; set; }
+        public string Description { get; set; }
+        public string Commentaire { get; set; }
+        public string StatutIncident { get; set; }
+        public string EntiteEnCharge { get; set; }
+        public ContactDto Contact { get; set; }
+    }
+}
